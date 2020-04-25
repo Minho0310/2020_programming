75c9a625f0d63b6cb23b13d19b4415098c5433a7
Minho0310 <63887984+Minho0310@users.noreply.github.com>
2020-04-25 12:59:26 +0900

    Add files via upload

---
+++ 0425/turtle01.py
@@ -0,0 +1,16 @@
+import turtle as t
+t.shape("turtle")
+t.write(t.position())
+t.forward(100)
+t.write(t.position())
+t.left(90)
+t.forward(100)
+t.write(t.position())
+
+t.left(120)
+t.forward(100)
+t.write(t.position())
+
+t.left(120)
+t.forward(100)
+t.write(t.position())
